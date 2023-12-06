5b301289118a884bb5210325467ae7d6dddbb4c8
KirstenWF <kirsten@wayfindergames.se>
2023-12-06 15:52:47 +0100

    handle dev key

--- LootLockerServerSDK/Source/LootLockerServerSDK/Private/ServerAPI/LootLockerServerAuthRequest.cpp
+++ LootLockerServerSDK/Source/LootLockerServerSDK/Private/ServerAPI/LootLockerServerAuthRequest.cpp
@@ -25,7 +25,7 @@ void ULootLockerServerAuthRequest::StartSession(const FLootLockerServerAuthRespo
 		{
 			ULootLockerServerStateData::SetServerToken(Response.Token);
 		}),
-		{ {"x-server-key", Config->LootLockerServerKey} }
+		{ {"x-server-key", Config->LootLockerServerKey}, { TEXT("is-development"), Config->LootLockerServerKey.StartsWith("dev_") ? TEXT("true") : TEXT("false") } }
 	);
 }
 
